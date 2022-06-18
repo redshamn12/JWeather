f97ff8ac61d624ba4f003b426d50340f9703f866
redshamn12 <9pvsatria@gmail.com>
2022-06-19 00:51:18 +0700

    Adding resjson

--- mainf/sweather/views.py
+++ mainf/sweather/views.py
@@ -1,9 +1,23 @@
 from django.shortcuts import render
+from decouple import config
+import json
+import urllib.request
 
 # Create your views here.
 def index(request):
     if request.method == 'POST':
         city = request.POST['city']
+        send = urllib.request.urlopen('http://api.openweathermap.org/data/2.5/weather?q='+city+'&appid='+config('OPEN_W_API')).read()
+        resJSON = json.loads(send)
+        data = {
+            "country_code": str(resJSON['sys']['country']),
+            "coordinate": str(resJSON['coord']['lon'])+ ' '+str(resJSON['coord']['lat']),
+            "temp": str(resJSON['main']['temp'])+'k',
+            "pressure": str(resJSON['main']['pressure']),
+            "humidity": str(resJSON['main']['humidity']),
+        }
     else:
-        city = ''
-    return render(request, 'index.html', {'city':city})
+        city = ' '
+        data = {}
+    return render(request,'index.html', {'city':city, 'data':data})
+    
