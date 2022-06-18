8aae8e7d7c196e4e97777171544ddea8eb8bfb30
redshamn12 <9pvsatria@gmail.com>
2022-06-19 01:18:10 +0700

    Adding post to call api

--- mainf/sweather/views.py
+++ mainf/sweather/views.py
@@ -5,14 +5,14 @@ import urllib.request
 
 # Create your views here.
 def index(request):
-    if request.method == 'POST':
+    if request.method == 'POST' and request.POST:
         city = request.POST['city']
-        send = urllib.request.urlopen('http://api.openweathermap.org/data/2.5/weather?q='+city+'&appid='+config('OPEN_W_API')).read()
+        send = urllib.request.urlopen('http://api.openweathermap.org/data/2.5/weather?q='+city+'&appid='+config('OPEN_W_API')+'&units=metric').read()
         resJSON = json.loads(send)
         data = {
             "country_code": str(resJSON['sys']['country']),
-            "coordinate": str(resJSON['coord']['lon'])+ ' '+str(resJSON['coord']['lat']),
-            "temp": str(resJSON['main']['temp'])+'k',
+            "coordinate": 'X '+str(resJSON['coord']['lon'])+ ' Y '+str(resJSON['coord']['lat']),
+            "temp": str(resJSON['main']['temp'])+' C'+u"\N{DEGREE SIGN}",
             "pressure": str(resJSON['main']['pressure']),
             "humidity": str(resJSON['main']['humidity']),
         }
