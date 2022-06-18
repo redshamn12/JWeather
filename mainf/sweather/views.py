b73787ce8a15458852e061d91be947a702b76e9d
redshamn12 <9pvsatria@gmail.com>
2022-06-19 01:22:11 +0700

    Adding route to 404.html

--- mainf/sweather/views.py
+++ mainf/sweather/views.py
@@ -20,4 +20,6 @@ def index(request):
         city = ' '
         data = {}
     return render(request,'index.html', {'city':city, 'data':data})
-    
+
+def page_not_found(request, exception):
+    return render(request, '404.html', status=404)
